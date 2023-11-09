0259d968440be26c9d950714028eda1edf293295
Lycan <guerrerodelanoche300@gmail.com>
2023-11-08 23:03:07 -0500

    wrote out acceptance criteria and initializers to begin program out

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   // ADD CODE HERE
+
+//Initializers
+var password = '';
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var upperCase = 'ABCDEFGHIJKLNOPQRSTUVWYXYZ';
+var symbols = '!@#$%^&*()+_';
+var numbers = '1234567890';
+var passwordLength = 8;
+
+//Acceptance Criteria
+//display password criteria for user to select after initial click
+//user inputs criteria for password such as characters used and length
+//once each criteria is selected a password will generate and displayed to page
+
+
 }
 
 // Write password to the #password input
