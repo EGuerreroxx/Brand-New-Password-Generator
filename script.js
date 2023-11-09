48577f776e3ba66f075513af76a8f54a42b40910
Lycan <guerrerodelanoche300@gmail.com>
2023-11-09 01:17:27 -0500

    All possible outcomes for character choices have been added as an if statement, logic is being worked on

--- script.js
+++ script.js
@@ -12,37 +12,79 @@ var upperCase = 'ABCDEFGHIJKLNOPQRSTUVWYXYZ';
 var symbols = '!@#$%^&*()+_';
 var numbers = '1234567890';
 
-
-var hasLowerCase = true;
-var hasUpperCase = true;
-var hasNumbers = true;
-var hasSymbols = true;
-
 //Acceptance Criteria
 //display password criteria for user to select after initial click
 //user inputs criteria for password such as characters used and length
 //once each criteria is selected a password will generate and displayed to page
 
 
-if(!(hasLowerCase && hasUpperCase && hasNumbers && hasSymbols)==0){
+//User prompts to determine which characters will be used
+var hasLowerCase = prompt("Would you like lowercase letters (Y/N)?");
+var hasUpperCase = prompt("Would you like uppercase letters (Y/N)?");
+var hasNumbers = prompt("Would you like numbers (Y/N)?");
+var hasSymbols = prompt("Would you like symbols (Y/N)?");
+
+//converting user input to UpperCase for if statement validation
+var hasLowerCase = hasLowerCase[0].toUpperCase();
+var hasUpperCase = hasUpperCase[0].toUpperCase();
+var hasNumbers = hasNumbers[0].toUpperCase();
+var hasSymbols = hasSymbols[0].toUpperCase();
+
+//If no character input is selected than an alert will pop up telling user to choose at least one character type to include
+if((hasLowerCase && hasUpperCase && hasNumbers && hasSymbols)==='N'){
   alert("Please choose at least one char type to include in your password!");
   return;
 }
 
-if(!hasLowerCase){
+//All possible character combinations in form of If statements
+if(hasLowerCase =="N"){
   var potentialChars = upperCase + numbers + symbols;
 
-}else if(!hasUpperCase){
+}else if(hasUpperCase == "N"){
   var potentialChars = lowerCase + numbers + symbols;
 
-}else if(!hasNumbers){
+}else if(hasNumbers == "N"){
   var potentialChars = lowerCase + upperCase + symbols;
 
-}else if(!hasSymbols){
-  var potentialChars = lowerCase + + upperCase + numbers;
+}else if(hasSymbols == "N"){
+  var potentialChars = lowerCase + upperCase + numbers;
 
+}else if((hasLowerCase && hasUpperCase) == "N"){
+  var potentialChars = numbers + symbols;
+
+}else if((hasLowerCase && hasNumbers) == "N"){
+  var potentialChars = upperCase + symbols;
+
+}else if((hasLowerCase && hasSymbols) == "N"){
+  var potentialChars = upperCase + numbers;
+
+}else if((hasUpperCase && hasNumbers) == "N"){
+  var potentialChars = lowerCase + symbols;
+
+}else if((hasUpperCase && hasSymbols) == "N"){
+  var potentialChars = lowerCase + numbers;
+
+}else if((hasNumbers && hasSymbols) == "N"){
+  var potentialChars = lowerCase + upperCase;
+
+}else if((hasUpperCase && hasNumbers && hasSymbols)== "N"){
+  var potentialChars = lowerCase;
+
+}else if((hasLowerCase && hasNumbers && hasSymbols)== "N"){
+  var potentialChars = upperCase;
+
+}else if((hasLowerCase && hasUpperCase && hasSymbols) == "N"){
+  var potentialChars = numbers;
+
+}else if ((hasLowerCase && hasUpperCase && hasNumbers) == "N"){
+  var potentialChars = symbols;
+
+}else{
+  var potentialChars = lowerCase + upperCase + numbers + symbols;
 }
+
 //User is prompted for length and if statement is created to satisfy 8-128 chars
+//potentialChars is used here from the previous If statement according to user input to have character picked at random
 var passwordLength = prompt ("Please input the length you would like your password to be!");
 if (passwordLength >= 8 && passwordLength <= 128) {
   for(x = 0; x < passwordLength; x++){
@@ -50,21 +92,11 @@ if (passwordLength >= 8 && passwordLength <= 128) {
   password += potentialChars.charAt(gen);
   }
 
-}else[
-  alert("Invalid range. Please choose a password length between 8 and 128!")
-]
-
-// prompt("Would you like lowercase letters?")
-// prompt("Would you like uppercase letters?")
-// prompt("Would you like numbers?")
-// prompt("Would you like symbols?")
-
-
-
+}else{//If User fails to choose an appropriate length an alert box will pop up asking user to choose an appropriate length
+  alert("Invalid range. Please choose a password length between 8 and 128!");
+}
 
 return password;
-
-
 }
 
 // Write password to the #password input
